c32afeea99a22edbc56dd0c508ce19d693d52c11
Michel Correa da Luz <michelcluz@gmail.com>
2021-12-22 16:16:07 -0300

    teste de request

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getHoroscopo(body: {signo: string}) {
+    let horoscopo;
+    if (body.signo == 'sagitário') {
+      horoscopo = "esse é dos bons!"
+    }
+    else {
+      horoscopo = "não é dos bons"
+    }
+    return horoscopo;
+  }
 }
